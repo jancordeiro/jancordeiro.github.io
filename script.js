e454c0b809d19c1f710b39459abcf80f99bdb94d
Jan Cordeiro <121439832+jancordeiro@users.noreply.github.com>
2024-04-23 09:11:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,20 +7,20 @@ modeToggle.addEventListener('click', function () {
 
 // Verificar qual modo está atualmente ativado
 if (body.classList.contains('light-mode')) {
-    // Se o modo claro estiver ativado, mude para o modo escuro
-    body.classList.remove('light-mode');
-    body.classList.add('dark-mode');
+// Se o modo claro estiver ativado, mude para o modo escuro
+    	body.classList.remove('light-mode');
+    	body.classList.add('dark-mode');
 	nav.classList.remove('bg-light', 'navbar-light');
 	nav.classList.add('bg-dark', 'navbar-dark');
-    modeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    	modeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
 	}
 
 else {
-    // Se o modo escuro estiver ativado, mude para o modo claro
-    body.classList.remove('dark-mode');
-    body.classList.add('light-mode');
+// Se o modo escuro estiver ativado, mude para o modo claro
+    	body.classList.remove('dark-mode');
+    	body.classList.add('light-mode');
 	nav.classList.remove('bg-dark', 'navbar-dark');
 	nav.classList.add('bg-light', 'navbar-light');
-    modeToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
+    	modeToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
 	}
 });
